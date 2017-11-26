5bc81917477a66b7fb8913c213cdd21ad28a4940
Pier-Yves Lessard <py.lessard@gmail.com>
2017-11-25 20:07:05 -0800

    Updated setup with classifier

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from distutils.core import setup
 setup(
   name = 'can-isotp',
   packages = ['can-isotp'],
-  version = '1.0',
+  version = '0.1',
   description = 'Wrapper for Python 3.7+ simplifying the usage of Oliver Herktopp\'s Linux kernel module enabling ISO-15765 sockets',
   author = 'Pier-Yves Lessard',
   author_email = 'py.lessard@gmail.com',
   url = 'https://github.com/pylessard/python-can-isotp',
-  download_url = 'https://github.com/pylessard/python-can-isotp/archive/v1.0.tar.gz',
+  download_url = 'https://github.com/pylessard/python-can-isotp/archive/v0.1.tar.gz',
   keywords = ['isotp', 'can', 'iso-15765', '15765', 'iso15765'], 
-  classifiers = [],
+  classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        ],
 )
