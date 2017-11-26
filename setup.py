b1168cfc758deb842431c244864c2f47662c3f35
Pier-Yves Lessard <py.lessard@gmail.com>
2017-11-25 20:20:32 -0800

    Updated release files

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'can-isotp',
-  packages = ['can-isotp'],
-  version = '0.1',
+  packages = ['isotp'],
+  version = '0.1.0',
   description = 'Wrapper for Python 3.7+ simplifying the usage of Oliver Herktopp\'s Linux kernel module enabling ISO-15765 sockets',
+  long_description=long_description,
   author = 'Pier-Yves Lessard',
   author_email = 'py.lessard@gmail.com',
+  license='MIT',
   url = 'https://github.com/pylessard/python-can-isotp',
   download_url = 'https://github.com/pylessard/python-can-isotp/archive/v0.1.tar.gz',
   keywords = ['isotp', 'can', 'iso-15765', '15765', 'iso15765'], 
+  python_requires='>=3.7',
   classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
