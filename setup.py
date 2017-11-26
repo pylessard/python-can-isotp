233e84f2b4a6dc40b527b4a68b597d046f79cafc
Pier-Yves Lessard <py.lessard@gmail.com>
2017-11-25 19:49:03 -0800

    Added PyPI required files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'can-isotp',
+  packages = ['can-isotp'],
+  version = '1.0',
+  description = 'Wrapper for Python 3.7+ simplifying the usage of Oliver Herktopp\'s Linux kernel module enabling ISO-15765 sockets',
+  author = 'Pier-Yves Lessard',
+  author_email = 'py.lessard@gmail.com',
+  url = 'https://github.com/pylessard/python-can-isotp',
+  download_url = 'https://github.com/pylessard/python-can-isotp/archive/v1.0.tar.gz',
+  keywords = ['isotp', 'can', 'iso-15765', '15765', 'iso15765'], 
+  classifiers = [],
+)
