929a0fd3d71afc97495b9a3582b393df0eb22268
Stefan Rupp <stefan.rupp@digital-workbench.de>
2018-04-27 17:20:25 +0200

    fix for TX and RX padding

--- isotp/__init__.py
+++ isotp/__init__.py
@@ -152,11 +152,13 @@ class opts:
                 if not isinstance(txpad, int) or txpad<0 or txpad>0xFF:
                     raise ValueError("txpad must be a an integer between 0 and FF")
                 o.txpad = txpad
+                o.optflag |= opts.flags.TX_PADDING
 
             if rxpad != None:
                 if not isinstance(rxpad, int) or rxpad<0 or rxpad>0xFF:
                     raise ValueError("rxpad must be a an integer between 0 and FF")
                 o.rxpad = rxpad
+                o.optflag |= opts.flags.RX_PADDING
 
             if rx_ext_address != None:
                 if not isinstance(rx_ext_address, int) or rx_ext_address<0 or rx_ext_address>0xFF:
