e139766b946589b565c2c9ae4870f1d34055e0bf
Pier-Yves Lessard <py.lessard@gmail.com>
2019-01-01 20:08:08 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
   name = 'can-isotp',
-  packages = ['isotp'],
-  version = '1.0',
+  packages = find_packages(exclude=['test']),
+  version = '1.3',
   description = 'Module enabling the IsoTP protocol defined by ISO-15765',
   long_description=long_description,
   author = 'Pier-Yves Lessard',
   author_email = 'py.lessard@gmail.com',
   license='MIT',
   url = 'https://github.com/pylessard/python-can-isotp',
-  download_url = 'https://github.com/pylessard/python-can-isotp/archive/v0.4.tar.gz',
+  download_url = 'https://github.com/pylessard/python-can-isotp/archive/v1.3.tar.gz',
   keywords = ['isotp', 'can', 'iso-15765', '15765', 'iso15765'], 
   python_requires='>=3',
   classifiers = [
