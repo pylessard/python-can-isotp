fc8037cfd5e5e70618670b109dedb7d66ee6e2e7
Christophe PAVOT <christophe.pavot@gmail.com>
2019-06-27 14:21:02 +0200

    Add unit tests for ll_data_length CAN Stack parameter

--- test/test_layer_vs_socket.py
+++ test/test_layer_vs_socket.py
@@ -27,15 +27,42 @@ class TestLayerAgainstSocket(ThreadableTest):
     def setUp(self):
         self.socket_list = []
 
-    def make_socket(self, *args, **kwargs):
+    def make_socket(self, ll_data_length=8, *args, **kwargs):
+        if tools.get_test_interface_config("fd"):
+            mtu = isotp.tpsock.LinkLayerProtocol.CAN_FD
+        else:
+            mtu = isotp.tpsock.LinkLayerProtocol.CAN
+
+        if mtu == isotp.tpsock.LinkLayerProtocol.CAN:
+            assert ll_data_length == 8, "CAN bus only supports 8-bytes payloads"
+
         s = isotp.socket(*args, **kwargs)
+        s.set_ll_opts(mtu=mtu, tx_dl=ll_data_length)
         self.socket_list.append(s)
         return s
 
+    def get_can_stack(self, **isotp_params_overload):
+        # Default configuration
+        isotp_params = {
+            'stmin': 10,  # Will request the sender to wait 10ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
+            'blocksize': 48,  # Request the sender to send 48 consecutives frames before sending a new flow control message
+            'wftmax': 0,  # Number of wait frame allowed before triggering an error
+            'll_data_length': 8,  # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
+            'tx_padding': 0,  # Will pad all transmitted CAN messages with byte 0x00. None means no padding
+            'rx_flowcontrol_timeout': 1000,  # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
+            'rx_consecutive_frame_timeout': 1000,  # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
+            'squash_stmin_requirement': False,  # When sending, respect the stmin requirement of the receiver. If set to True, go as fast as possible.
+            'is_fd': tools.get_test_interface_config("fd")
+        }
+
+        isotp_params.update(isotp_params_overload)
+
+        return isotp.CanStack(bus=self.bus, address=self.address, params=isotp_params)
+
     def clientSetUp(self):
-        self.bus = can.interface.Bus(tools.get_test_interface(), bustype='socketcan')
-        address = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=self.stack_txid, rxid=self.stack_rxid)
-        self.stack = isotp.CanStack(bus=self.bus, address=address)
+        self.bus = can.interface.Bus(**tools.get_test_interface_config())
+        self.address = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=self.stack_txid, rxid=self.stack_rxid)
+        self.stack = self.get_can_stack()
 
     def tearDown(self):
         for socket in self.socket_list:
@@ -80,11 +107,12 @@ class TestLayerAgainstSocket(ThreadableTest):
             time.sleep(0.05)
         
 
-
     # ========= Test cases ======
-    def test_receive(self):
-        s = self.make_socket()
-        s.bind(tools.get_test_interface(), txid=self.stack_rxid, rxid=self.stack_txid)
+    def _test_receive_ll_data_length(self, ll_data_length=8, remote_ll_data_length=8):
+        # Need a new CAN stack with ll_data_length parametrization
+        self.stack = self.get_can_stack(ll_data_length=ll_data_length)
+        s = self.make_socket(ll_data_length=remote_ll_data_length)
+        s.bind(tools.get_test_interface_config("channel"), txid=self.stack_rxid, rxid=self.stack_txid)
         self.socket_ready = True
         s.send(b'a'*100)
         self.wait_reception_complete()
@@ -94,9 +122,38 @@ class TestLayerAgainstSocket(ThreadableTest):
         frame = self.process_stack_receive()
         self.assertEqual(frame, b'a'*100)
 
-    def test_transmit(self):
-        s = self.make_socket()
-        s.bind(tools.get_test_interface(), txid=self.stack_rxid, rxid=self.stack_txid)
+    def test_receive_ll_data_length_8_8(self):
+        return self._test_receive_ll_data_length(ll_data_length=8, remote_ll_data_length=8)
+
+    def _test_receive_ll_data_length_8_8(self):
+        return self._test_receive()
+
+    def test_receive_ll_data_length_64_64(self):
+        assert tools.get_test_interface_config("fd") is True, "This test requires to use a CAN-FD bus"
+        return self._test_receive_ll_data_length(ll_data_length=64, remote_ll_data_length=64)
+
+    def _test_receive_ll_data_length_64_64(self):
+        return self._test_receive()
+
+    def test_receive_ll_data_length_8_64(self):
+        assert tools.get_test_interface_config("fd") is True, "This test requires to use a CAN-FD bus"
+        return self._test_receive_ll_data_length(ll_data_length=8, remote_ll_data_length=64)
+
+    def _test_receive_ll_data_length_8_64(self):
+        return self._test_receive()
+
+    def test_receive_ll_data_length_64_8(self):
+        assert tools.get_test_interface_config("fd") is True, "This test requires to use a CAN-FD bus"
+        return self._test_receive_ll_data_length(ll_data_length=64, remote_ll_data_length=8)
+
+    def _test_receive_ll_data_length_64_8(self):
+        return self._test_receive()
+
+    def _test_transmit_ll_data_length(self, ll_data_length=8, remote_ll_data_length=8):
+        # Need a new CAN stack with ll_data_length parametrization
+        self.stack = self.get_can_stack(ll_data_length=ll_data_length)
+        s = self.make_socket(ll_data_length=remote_ll_data_length)
+        s.bind(tools.get_test_interface_config("channel"), txid=self.stack_rxid, rxid=self.stack_txid)
         self.socket_ready = True
 
         self.wait_transmission_complete(1)
@@ -108,10 +165,37 @@ class TestLayerAgainstSocket(ThreadableTest):
         self.stack.send(b'b'*100)
         self.process_stack_send()
         
+    def test_transmit_ll_data_length_8_8(self):
+        return self._test_transmit_ll_data_length(ll_data_length=8, remote_ll_data_length=8)
+
+    def _test_transmit_ll_data_length_8_8(self):
+        return self._test_transmit()
+
+    def test_transmit_ll_data_length_64_64(self):
+        assert tools.get_test_interface_config("fd") is True, "This test requires to use a CAN-FD bus"
+        return self._test_transmit_ll_data_length(ll_data_length=64, remote_ll_data_length=64)
+
+    def _test_transmit_ll_data_length_64_64(self):
+        return self._test_transmit()
+
+    def test_transmit_ll_data_length_8_64(self):
+        assert tools.get_test_interface_config("fd") is True, "This test requires to use a CAN-FD bus"
+        return self._test_transmit_ll_data_length(ll_data_length=8, remote_ll_data_length=64)
+
+    def _test_transmit_ll_data_length_8_64(self):
+        return self._test_transmit()
+
+    def test_transmit_ll_data_length_64_8(self):
+        assert tools.get_test_interface_config("fd") is True, "This test requires to use a CAN-FD bus"
+        return self._test_transmit_ll_data_length(ll_data_length=64, remote_ll_data_length=8)
+
+    def _test_transmit_ll_data_length_64_8(self):
+        return self._test_transmit()
+
     def test_transmit_long_stmin(self):
         s = self.make_socket()
         s.set_fc_opts(stmin=100)
-        s.bind(tools.get_test_interface(), txid=self.stack_rxid, rxid=self.stack_txid)
+        s.bind(tools.get_test_interface_config("channel"), txid=self.stack_rxid, rxid=self.stack_txid)
         self.socket_ready = True
         self.wait_transmission_complete(5)
         frame = s.recv()
@@ -130,7 +214,7 @@ class TestLayerAgainstSocket(ThreadableTest):
 
     def test_receive_long_stmin(self):
         s = self.make_socket()
-        s.bind(tools.get_test_interface(), txid=self.stack_rxid, rxid=self.stack_txid)
+        s.bind(tools.get_test_interface_config("channel"), txid=self.stack_rxid, rxid=self.stack_txid)
         self.socket_ready = True
         s.send(b'a'*150)
         self.wait_reception_complete(timeout=5)
@@ -150,7 +234,7 @@ class TestLayerAgainstSocket(ThreadableTest):
     def test_receive_extended_29bits(self):
         s = self.make_socket()
         addr = isotp.Address(isotp.AddressingMode.Extended_29bits, txid=self.stack_rxid, rxid=self.stack_txid, source_address=0x88, target_address=0x99)
-        s.bind(tools.get_test_interface(),addr)
+        s.bind(tools.get_test_interface_config("channel"),addr)
         self.socket_ready = True
         s.send(b'a'*100)
         self.wait_reception_complete()
@@ -165,7 +249,7 @@ class TestLayerAgainstSocket(ThreadableTest):
     def test_transmit_extended_29bits(self):
         s = self.make_socket()
         addr = isotp.Address(isotp.AddressingMode.Extended_29bits, txid=self.stack_rxid, rxid=self.stack_txid, source_address=0x88, target_address=0x99)
-        s.bind(tools.get_test_interface(), addr)
+        s.bind(tools.get_test_interface_config("channel"), addr)
         self.socket_ready = True
 
         self.wait_transmission_complete(1)
@@ -182,7 +266,7 @@ class TestLayerAgainstSocket(ThreadableTest):
     def test_receive_mixed_29bits(self):
         s = self.make_socket()
         addr = isotp.Address(isotp.AddressingMode.Mixed_29bits, source_address=0x88, target_address=0x99, address_extension=0xDD)
-        s.bind(tools.get_test_interface(),addr)
+        s.bind(tools.get_test_interface_config("channel"),addr)
         self.socket_ready = True
         s.send(b'c'*100)
         self.wait_reception_complete()
@@ -197,7 +281,7 @@ class TestLayerAgainstSocket(ThreadableTest):
     def test_transmit_mixed_29bits(self):
         s = self.make_socket()
         addr = isotp.Address(isotp.AddressingMode.Mixed_29bits, source_address=0x88, target_address=0x99, address_extension=0xEE)
-        s.bind(tools.get_test_interface(), addr)
+        s.bind(tools.get_test_interface_config("channel"), addr)
         self.socket_ready = True
 
         self.wait_transmission_complete(1)
--- test/tools.py
+++ test/tools.py
@@ -1,9 +1,16 @@
 class Vars:
     SOCKET_IMPOSSIBLE_REASON = ''
-    interface_name = 'vcan0'
+    interface_config = {
+        "interface": "socketcan",
+        "channel": "vcan0",
+        "fd": True,
+        "bitrate": 500000,
+        "databitrate": 4000000
+    }
     ISOTP_SOCKET_POSSIBLE = False
     
     next_id = 10
+
 
 def check_isotp_socket_possible():
     try:
@@ -11,7 +18,7 @@ def check_isotp_socket_possible():
         import isotp
         import can
         s = isotp.socket()
-        s.bind(Vars.interface_name, rxid=1, txid=2)
+        s.bind(get_test_interface_config("channel"), rxid=1, txid=2)
         s.close()
         Vars.ISOTP_SOCKET_POSSIBLE = True
     except Exception as e:
@@ -20,11 +27,17 @@ def check_isotp_socket_possible():
 
     return Vars.ISOTP_SOCKET_POSSIBLE
 
+
 def isotp_socket_impossible_reason():
     return Vars.SOCKET_IMPOSSIBLE_REASON
 
-def get_test_interface():
-    return Vars.interface_name
+
+def get_test_interface_config(parameter_name=None):
+    if parameter_name:
+        return Vars.interface_config[parameter_name]
+    else:
+        return Vars.interface_config
+
 
 def get_next_can_id_pair():
     pair = (Vars.next_id, Vars.next_id+1)
