fd6340e665ca12224d7df66796bc1d778a70419b
Pier-Yves Lessard <py.lessard@gmail.com>
2024-01-03 00:36:01 -0500

    Removed debug

--- test/unittest_logging.py
+++ test/unittest_logging.py
@@ -7,7 +7,7 @@ handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(level=logging.DEBUG)
-logger.disabled = False
+logger.disabled = True
 
 
 def configure_transport_layer(layer: isotp.TransportLayer):
