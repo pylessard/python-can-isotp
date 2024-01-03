4d8b2943ffbb2f9910e5f77b4def8bb67d2ccfa0
Pier-Yves Lessard <py.lessard@gmail.com>
2024-01-02 22:05:56 -0500

    Prototype of asymetric address

--- isotp/__init__.py
+++ isotp/__init__.py
@@ -2,6 +2,6 @@ _major_version_ = 2
 
 from isotp.errors import *
 from isotp.can_message import CanMessage
-from isotp.address import AddressingMode, TargetAddressType, Address
+from isotp.address import AddressingMode, TargetAddressType, Address, AsymetricAddress
 from isotp.protocol import TransportLayerLogic, TransportLayer, CanStack, NotifierBasedCanStack
 from isotp.tpsock import socket
