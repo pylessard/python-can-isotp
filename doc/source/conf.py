9bfe34ca440f705e8282b501ac6e42b404038d1b
Pier-Yves Lessard <py.lessard@gmail.com>
2024-06-26 09:03:41 -0400

    typo in doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+from os.path import abspath, join, dirname, normpath
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
