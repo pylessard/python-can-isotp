89a475ad44622ada0c55be6da981eab72de23376
Pier-Yves Lessard <py.lessard@gmail.com>
2026-06-26 14:53:33 -0400

    ci runtest upgrade

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -5,10 +5,16 @@ COVERAGE_SUFFIX="${COVERAGE_SUFFIX:-dev}"
 HTML_COVDIR="htmlcov_${COVERAGE_SUFFIX}"
 COV_DATAFILE=".coverage_${COVERAGE_SUFFIX}"
 
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd -P )"
+VENV_DIR="${VENV_DIR:-venv}"
+VENV_ROOT="${VENV_DIR:-$PROJECT_ROOT/$VENV_DIR}"
+
 if ! [[ -z "${BUILD_CONTEXT+x}" ]]; then
     if [[ "$BUILD_CONTEXT" == "ci" ]]; then
         if ! [[ -z "${NODE_NAME+x}" ]]; then
             echo "Running tests on agent: ${NODE_NAME}"
+            export PIP_CACHE_DIR=$VENV_ROOT/pip_cache   # Avoid concurrent cache access issue on CI
+            echo "PIP_CACHE_DIR=${PIP_CACHE_DIR}"
         fi
     fi
 fi
